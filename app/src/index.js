a7aecf9f45205a1c06ef873983a14732fb02e021
Matthew Goldfield <mgoldfield@gmail.com>
2020-08-19 15:29:37 -0400

    loop controller

--- app/src/index.js
+++ app/src/index.js
@@ -4,75 +4,144 @@ import './index.css';
 
 
 class Looper extends React.Component {
-    render (){
+    render() {
         return (
-            <div className="looper" />
+            <div className="looper">
+                <MasterLoop />
+            </div>
+        );
+    }
+}
+
+class MasterLoop extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            "extensionVisible":false,
+            "expanded": false,
+        }
+    }
+
+    handleStop(){
+        return;
+    }
+
+    handlePlay(){
+        return;
+    }
+
+    handleRec(){
+        return;
+    }
+
+    handleExpand(){
+        let me = this;
+
+        return () => {
+            console.log("Expand was pressed");
+
+            me.setState({
+                "expanded": !me.state.expanded,
+                "extensionVisible": !me.state.extensionVisible,
+            });
+
+            // me.setState({
+            //     
+            // });
+        };
+    }
+
+    getExtensionClasses(){
+        let classes = "masterExtension ";
+        if (this.state.extensionVisible){
+            classes +=  "visibleExtension";
+        }
+
+        return classes;
+    }
+
+    render() {
+        return (
+            <div className="masterLoop">
+                <div className="mainMasterLoop"> 
+                    <Button name="stop" onClick={this.handleStop} />
+                    <Button name="play" onClick={this.handlePlay} />
+                    <Button name="rec" onClick={this.handleRec} />
+                    <Button name="expand" onClick={this.handleExpand()} />
+                </div>
+                <div className={this.getExtensionClasses()}>
+                    <Button name="click" onClick={this.handleClick} />
+                </div>
+            </div>
         );
     }
 }
 
 
+class Loop extends React.Component {
+    handlePlay(){
+        return;
+    }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+    handleStop(){
+        return;
+    }
+
+    render() {
+        return (
+            <div className="loop"> 
+                <Button name="stop" onClick={this.handleStop} />
+                <Button name="play" onClick={this.handlePlay} />
+            </div>
+        );
+    }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
+function Button(props) {
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        <div className="button" onClick={props.onClick}>
+            {props.name}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
     );
-  }
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+class Slider extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {"value":null};
+    }
+
+    onInput(event){
+        this.setState({value:event.target.value})
+    }
+
+    onChange(event){
+        return;
+    }
+
+    render() {
+        return (
+            <div className="slider">
+                <input 
+                    type="range" 
+                    min={this.props.min} 
+                    max={this.props.max}
+                    value={this.props.value}
+                    onChange={this.props.onChange}
+                    onInput={this.onInput}
+                />
+                <span name="sliderValBox">
+                    {this.state.value}
+                </span>
+            </div>
+
+        );
+    }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Looper />,
-  document.getElementById('root')
+    <Looper />,
+    document.getElementById('root')
 );
