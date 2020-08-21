c0caa1a3c266140e47071e26c138244516be2296
Matthew Goldfield <mgoldfield@gmail.com>
2020-08-21 12:24:16 -0400

    break up files for readability

---
+++ app/src/controls.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+class Button extends React.Component {
+
+    classes(){
+        let extraClasses = {
+            'expand': ['downarrow'],
+            'collapse': ['uparrow'],
+        }
+
+        let classList = ['button'].concat(extraClasses[this.props.name] || []);
+        if (this.props.toggled) classList.push('toggled');
+
+        return classList.join(' ');
+    }
+
+    hideText(name){
+        let hide = {
+            'expand': true,
+            'collapse': true,
+        }
+
+        return hide[name] || false;
+    }
+
+    render() {
+        return (
+            <div className={this.classes()} onClick={this.props.onClick}>
+                {(this.hideText(this.props.name)) ? '' : this.props.name}
+            </div>
+        );
+    }
+}
+
+class Slider extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {'value':this.props.value};
+    }
+
+    onChange(event){
+        return;
+    }
+
+    render() {
+        return (
+            <div className='slider'>
+                <div className='sliderTitle'>{this.props.name}</div>
+                <input 
+                    className='sliderInput'
+                    type='range' 
+                    min={this.props.min} 
+                    max={this.props.max}
+                    value={this.state.value}
+                    step={(this.props.step) ? this.props.step : 1}
+                    onChange={this.props.onChange}
+                    onInput={(e) => this.setState({'value':e.target.value})}
+                />
+                <span className='sliderValBox'>
+                    {this.state.value}
+                </span>
+            </div>
+
+        );
+    }
+}
+
+
+export {Button, Slider};
---
+++ app/src/looper.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+import {Button, Slider} from './controls.js';
+
+
+class Looper extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            'expanded': false,
+            'recording': false,
+            'playing': false,
+            'clicking': false,
+            'tempo': 60,
+        }
+        this.loops = []
+
+
+        this.handleStop = () => null;
+        this.handlePlay = () => {
+            this.setState({'playing': !this.state.playing});
+        }
+        this.handleRec = () => {
+            if (this.state.recording){
+                if (this.state.playing) this.pressStop(); else this.loops.pop();
+            }else{
+                let name = "Loop " + this.loops.length.toString();
+                this.loops.push(<Loop 
+                    key={name}
+                    name={name}
+                    recording={true}
+                    onChange={() => 2}
+                />);
+            }
+
+            this.setState({'recording': !this.state.recording});
+        }        
+    }
+
+    render() {
+        return (
+            <div className='looper'>
+                <div className='masterLoop'>
+                    <div className='mainMasterLoop'> 
+                        <Button name='stop' onClick={this.handleStop} />
+                        <Button name='play' onClick={this.handlePlay} toggled={this.state.playing} />
+                        <Button name='rec' onClick={this.handleRec} toggled={this.state.recording} />
+                        <Button 
+                            name={(this.state.expanded) ? 'collapse' : 'expand'}
+                            onClick={() => this.setState({'expanded': !this.state.expanded})} 
+                        />
+                    </div>
+                    <div className={'masterExtension '.concat((this.state.expanded) ? 'visibleExtension' : '')}>
+                        <Button name='turn click on' onClick={this.handleClick} toggled={this.state.clicking} />
+                        <Slider 
+                            name='tempo' min='30' max='200' 
+                            value={this.state.tempo} 
+                            onChange={(e) => this.setState({'tempo': e.target.value})}
+                        />
+                    </div>
+                </div>
+                <div class="fade-in">
+                    <ul className='loopList'>{this.loops}</ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Loop extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            'name': props.name,
+            'muted': false,
+            'recording': this.props.recording,
+            'gain': 1,
+        }
+
+        this.handleMute = () => this.setState({'muted': !this.state.muted})
+    }
+
+    render() {
+        return (
+            <li class="loopItem">
+            <div className='loop'> 
+                <input type='text' value={this.state.name} onChange={this.props.onChange}/>
+                <Button name='mute' onClick={this.handleMute} toggled={this.state.muted}/>
+                <Slider 
+                    name='gain' min='0' max='2' 
+                    value={this.state.gain} 
+                    onChange={(e) => this.setState({'gain': e.target.value})}
+                    step="0.01"
+                />                
+            </div>
+            </li>
+        );
+    }
+}
+
+export default Looper;
+
